e7d7215b80f36935637575fb06dee9a8b5d1f7e8
Pavel Frolov <artyhere@gmail.com>
2021-05-04 00:24:22 +0300

    add Dockerfile

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       v-model="version"
       v-on:clearOrder="clearOrder"
       v-on:reloadCatalog="reloadCatalog"
+      v-on:sendOrder="sendOrder"
     />
     <the-order v-if="version" :specs="specs" :order="order" />
   </div>
@@ -14,7 +15,14 @@
 import TheHeader from "./components/TheHeader.vue";
 import TheOrder from "./components/TheOrder.vue";
 
-var CATALOG_URL = "http://192.168.1.154:18084";
+var CATALOG_URL = process.env.CATALOG_URL ? process.env.CATALOG_URL : "http://localhost:18084";
+var TMF622_URL = process.env.TMF622_URL ? process.env.TMF622_URL : "http://localhost:18081";
+
+var DEFAULT_CUSTOMER = {
+  "@referredType": "Customer",
+  role: "Customer",
+  id: "default_toe_customer",
+};
 
 var CATALOG_STORAGE_KEY = "order-entry-catalog-vuejs-2.0";
 var catalogSnapshotStorage = {
@@ -79,7 +87,30 @@ export default {
     reloadCatalog: function () {
       fetch(CATALOG_URL + "/api/v1/om-ordering-catalog/catalog/snapshot")
         .then((response) => response.json())
-        .then((data) => (this.catalogSnapshot = data));
+        .then((data) => (this.catalogSnapshot = data))
+        .catch((error) => {
+          alert("Error:", error);
+        });
+    },
+    sendOrder: function () {
+      if (!this.order.relatedParty || this.order.relatedParty.length == 0) {
+        this.order.relatedParty = [DEFAULT_CUSTOMER];
+      }
+      fetch(TMF622_URL + "/productOrderingManagement/v1/productOrder", {
+        method: "POST",
+        headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.order),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          alert("Success:", data);
+        })
+        .catch((error) => {
+          alert("Error:", error);
+        });
     },
   },
   watch: {
